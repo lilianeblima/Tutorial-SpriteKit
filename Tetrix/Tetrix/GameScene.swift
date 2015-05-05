41f91d369bfd8a6f93db0d6435c71bf59d3a11f7
Liliane Bezerra Lima <liliane.b.lima@outlook.com>
2015-05-05 12:03:39 -0300

    Iniciando Terceiro Tutorial

---
+++ Tetrix/Tetrix/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Tetrix
+//
+//  Created by Liliane Bezerra Lima on 05/05/15.
+//  Copyright (c) 2015 Liliane Bezerra Lima. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ Tetrix/Tetrix/GameViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GameViewController.swift
+//  Tetrix
+//
+//  Created by Liliane Bezerra Lima on 05/05/15.
+//  Copyright (c) 2015 Liliane Bezerra Lima. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
